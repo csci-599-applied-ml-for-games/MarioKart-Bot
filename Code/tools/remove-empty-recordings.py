4b74b98d1118ac5354c2bbff91aca824acbcd7f5
Harsh Waghela <waghela@usc.edu>
2020-04-27 23:49:58 -0700

    Final Code

--- Code/tools/remove-empty-recordings.py
+++
@@ -1,6 +0,0 @@
-import glob, os
-for recording in glob.iglob("recordings/*/*/*"):
-    if os.listdir(recording) == ["steering.txt"]:
-        print(recording, "is empty. Removing...")
-        os.remove(os.path.join(recording, 'steering.txt'))
-        os.rmdir(recording)
